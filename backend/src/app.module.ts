f279b0b1cd30b221365f8e90665e17beb982b28c
Arosha Withanage <avwithanage2006@gmail.com>
2025-08-11 13:35:00 +0530

    feat: fix prisma and cros error

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './public/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
