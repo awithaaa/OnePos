1cacdd739d497aa6d7916c1ccbde282c926ad6d0
Arosha Withanage <avwithanage2006@gmail.com>
2025-08-11 10:20:19 +0530

    feat: add prisma to bn

---
+++ backend/prisma/migrations/20250811044702_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'cashier',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
