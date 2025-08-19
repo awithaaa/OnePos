495f5227965c11d28a2d427db97795583f801a86
Arosha Withanage <avwithanage2006@gmail.com>
2025-08-19 20:01:16 +0530

    feat: update close and delete in item

--- frontend/src/dashboard/store/item/Item.tsx
+++ frontend/src/dashboard/store/item/Item.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, redirect, useParams } from "react-router-dom";
 import { api } from "../../../services/api";
 import arrow_left from "../../../assets/arrow_left.svg";
 import EditItem from "../../../components/Edit-Item";
@@ -19,6 +19,11 @@ export default function ItemDetial() {
     fetchItem();
   }, []);
 
+  const deleteItem = async () => {
+    const res = await api.delete(`/items/${id}`);
+    redirect("/dashboard/store");
+  };
+
   if (isItem) {
     return (
       <>
@@ -35,29 +40,59 @@ export default function ItemDetial() {
 
           <div className="flex justify-center mt-10">
             <div className="bg-white p-6 rounded-xl">
-              <div className="flex justify-end gap-4">
-                <button
-                  className="bg-white rounded-full w-8 h-8 border-2 border-black p-1 cursor-pointer"
-                  onClick={() => setEdit(true)}
-                >
-                  <img src={EditIcon} alt="edit" className="w-5" />
-                </button>
-                <button className="bg-black rounded-full w-8 h-8 border-2 border-black p-1 cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="20px"
-                    viewBox="0 -960 960 960"
-                    width="20px"
-                    fill="#FFFFFF"
-                  >
-                    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
-                  </svg>
-                </button>
-              </div>
+              {isEdit && (
+                <div>
+                  <div className="flex justify-end gap-4">
+                    <button
+                      className="bg-black rounded-full w-8 h-8 border-2 border-black p-1 cursor-pointer"
+                      onClick={() => setEdit(false)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20px"
+                        viewBox="0 -960 960 960"
+                        width="20px"
+                        fill="#FFFFFF"
+                      >
+                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                      </svg>
+                    </button>
+                  </div>
 
-              <div className="w-full h-0.5 mt-4 mb-2 bg-neutral-200"></div>
-              {isEdit && <EditItem item={isItem} />}
-              {!isEdit && <ItemDetailBox item={isItem} />}
+                  <div className="w-full h-0.5 mt-4 mb-2 bg-neutral-200"></div>
+                  <EditItem item={isItem} />
+                </div>
+              )}
+
+              {!isEdit && (
+                <div>
+                  <div className="flex justify-end gap-4">
+                    <button
+                      className="bg-white rounded-full w-8 h-8 border-2 border-black p-1 cursor-pointer"
+                      onClick={() => setEdit(true)}
+                    >
+                      <img src={EditIcon} alt="edit" className="w-5" />
+                    </button>
+                    <button
+                      className="bg-black rounded-full w-8 h-8 border-2 border-black p-1 cursor-pointer"
+                      onClick={deleteItem}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20px"
+                        viewBox="0 -960 960 960"
+                        width="20px"
+                        fill="#FFFFFF"
+                      >
+                        <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                      </svg>
+                    </button>
+                  </div>
+
+                  <div className="w-full h-0.5 mt-4 mb-2 bg-neutral-200"></div>
+                  <ItemDetailBox item={isItem} />
+                </div>
+              )}
             </div>
           </div>
         </div>
