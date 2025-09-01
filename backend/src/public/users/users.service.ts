8f1b04afb2f999f6587e23f7cb186b24a0e93153
Arosha Withanage <avwithanage2006@gmail.com>
2025-09-01 14:05:49 +0530

    feat: fix user ps issue

--- backend/src/public/users/users.service.ts
+++ backend/src/public/users/users.service.ts
@@ -24,23 +24,44 @@ export class UsersService {
     });
   }
 
+  protected userWithOutPass(user: any) {
+    const { password, ...rest } = user;
+    return rest;
+  }
+
   async getUsers(start?: number, size?: number) {
     if (start !== undefined && size !== undefined) {
       const count = await this.prisma.user.count();
       const users = await this.prisma.user.findMany({
         skip: start,
         take: size,
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          role: true,
+          email: true,
+        },
       });
       return { count, users };
     }
     const count = await this.prisma.user.count();
-    const users = await this.prisma.user.findMany({});
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        role: true,
+        email: true,
+      },
+    });
     return { count, users };
   }
 
   async getUsersById(id: number) {
-    const user = await this.prisma.user.findUnique({ where: { id: id } });
-    if (!user) throw new NotFoundException('User not found!');
+    let userWp = await this.prisma.user.findUnique({ where: { id: id } });
+    if (!userWp) throw new NotFoundException('User not found!');
+    const { password, ...user } = userWp;
     const count = 1;
     return { user, count };
   }
@@ -62,6 +83,13 @@ export class UsersService {
             },
           },
         ],
+      },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        role: true,
+        email: true,
       },
     });
 
